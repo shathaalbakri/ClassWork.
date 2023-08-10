1e1e3b4ea006f5bd7da8d95af4c7a879599b87a8
shathaalbakri <141655462+shathaalbakri@users.noreply.github.com>
2023-08-10 14:04:55 +0400

    Add files via upload

---
+++ 1/index.js
@@ -0,0 +1,67 @@
+// var user={
+//     Name: "Shatha",
+//     Age: 23,
+//      address: "Oman"
+//  }
+//  console.log(user.Name +" "+ user.Age +"\n "+ user.address) 
+ 
+ 
+ 
+ 
+//  var users={
+//      name:"Ali",
+//      age:22,
+//      mobail:Math.floor(Math.random()*10000000000)
+//  }
+ 
+//  console.log(users.mobail)
+
+
+// var s={
+//     name:"shatha",
+//     age:22,
+//     mobail:Math.floor(Math.random()*10000000000),
+//     address:"Oman"
+// }
+// if (s.age >18)
+// {
+//     console.log(s.name)
+//     console.log(s.mobail)
+//     console.log(s.address)
+// }
+// else if (s.age <18)
+// {
+//     console.log("Thank You")
+// }
+// else
+// {
+//     console.log("Welcome")
+// }
+
+// var n={
+//     name:"shatha",
+//     age:22,
+//     address:"Oman"
+// }
+
+// if(n.name==="shatha" && n.age===20)
+// {
+//     console.log("Welcom")
+// }
+// else
+// {
+//     console.log("Thank You")
+// }
+
+// if(n.name==="shatha" && n.age===22)
+// {
+//     console.log("Welcom")
+// }
+// else
+// {
+//     console.log("Thank You")
+// }
+
+
+
+
