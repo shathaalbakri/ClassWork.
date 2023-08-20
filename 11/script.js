cd968c3679528aa0b5edcf301e03d6661f7a8029
shathaalbakri <141655462+shathaalbakri@users.noreply.github.com>
2023-08-20 19:13:23 +0400

    Add files via upload

---
+++ 11/script.js
@@ -0,0 +1,40 @@
+
+
+
+// let img=document.querySelector('img')
+// let sources = ['image/1.jpg','image/2.jpg','image/3.jpg']
+// let x=0
+
+// setInterval(function(){
+//     x++
+//     img.setAttribute("src", sources[x])
+//     if(x < sources.henght-1)
+//     {
+//         x=0;
+//     }   
+// },2000)
+
+// let parent =document.getElementById('parent')
+// let color =document.getElementsByClassName('color')
+// for(let i=0; i<color.length; i++)
+// {
+//     color[i].addEventListener('click',function(){
+//         {
+//             if(i==0)
+//             {
+//                 parent.style.background ="red"
+//                 parent.innerHTML="RED"
+//             }
+//             else if(i==1)
+//             {
+//                 parent.style.background ="green"
+//                 parent.innerHTML="GREEN"
+//             }
+//             else
+//             {
+//                 parent.style.background ="gold"
+//                 parent.innerHTML="GOLD"
+//             }
+//         }
+//     })
+// }
